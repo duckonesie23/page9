e2b2c776dd13b6547789f832414c9b91e1c94de3
mbondoc <maxbondoc11@gmail.com>
2024-02-07 14:37:48 -0500

    woah
    qwhat

---
+++ page9.java
@@ -0,0 +1,18 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: Max Bondoc
+*Date: February, 2024
+*Program Name: page9.java
+*************************************/
+import java.io.*;
+public class page9
+{
+    public static void main (String[ ] args)
+    {
+    System.out.println("Hi"); //message to user
+    System.out.println("LabOneA"); //assignment
+    }
+}
+/* if you want to write a detailed comment, and it
+wraps around the screen, use this block style of
+commenting. */
